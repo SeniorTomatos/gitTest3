a7406bdc6c7ae646c44fbb34e581247d99271f48
SeniorTomatos <dehuckoo5@gmail.com>
2022-07-18 16:45:33 +0300

    добавил !!!

--- src/main/java/com/startois/lesson4/HomeWork4.java
+++ src/main/java/com/startois/lesson4/HomeWork4.java
@@ -2,6 +2,6 @@ package com.startois.lesson4;
 
 public class HomeWork4 {
     public static void main(String[] args) {
-        System.out.println("делаем новую ветку без мёрджа предыдущей");
+        System.out.println("делаем новую ветку без мёрджа предыдущей!!!");
     }
 }
